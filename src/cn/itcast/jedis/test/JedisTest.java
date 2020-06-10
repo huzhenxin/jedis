57677b16c69d72269e023c7617374bebc974a6a6
huzhenxin <hzhenxin@163.com>
2020-06-10 10:33:33 +0800

    windows 2

--- src/cn/itcast/jedis/test/JedisTest.java
+++ src/cn/itcast/jedis/test/JedisTest.java
@@ -7,6 +7,8 @@ package cn.itcast.jedis.test;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 public class JedisTest {
     /*
     * 快速入门
@@ -32,6 +34,21 @@ public class JedisTest {
         //可以使用setex方法存储可以指定过期时间的key value
         jedis.setex("activecode",20,"hehe");
         // 3. 关闭连接
+        jedis.close();
+    }
+
+    @Test
+    public void test4(){
+        /*1. 获取连接*/
+        Jedis jedis = new Jedis();
+        // 2. 操作
+        // list存储
+        jedis.lpush("mylist","a","b","c");
+        jedis.rpush("mylist","a","b","c");
+
+        //list范围获取
+        List<String> mylist = jedis.lrange("mylist", 0, -1);
+        System.out.println(mylist);
         jedis.close();
     }
 }
